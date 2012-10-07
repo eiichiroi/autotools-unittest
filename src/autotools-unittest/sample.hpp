03896b614160813c354126d04cf76a18571e21f0
Eiichiro Iwata <eiichiro.iwata+github@gmail.com>
2012-10-08 01:31:00 +0900

    Supported the feature generating coverage reports by using lcov(gcov)
    If lcov is installed in your environment, you can generage coverage reports like this:
    
      $ ./configure --enable-coverage
      $ make coverage

--- src/autotools-unittest/sample.hpp
+++ src/autotools-unittest/sample.hpp
@@ -1,1 +1,9 @@
 int square(int n);
+
+inline int factorial(int n)
+{
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n-1);
+}
--- test/sample_test.cpp
+++ test/sample_test.cpp
@@ -7,3 +7,7 @@ TEST(square, seven)
   EXPECT_EQ(49, square(7));
 }
 
+TEST(factorial, ten)
+{
+  EXPECT_EQ(3628800, factorial(10));
+}
